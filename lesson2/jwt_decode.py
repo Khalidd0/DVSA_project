1c8187bbd33b7676ab8222360763ffd394ba95b9
Khalidd0 <khaledasm008@gmail.com>
2026-04-17 02:41:19 +0300

    Refactor JWT decoding to handle multiple tokens

--- lesson2/jwt_decode.py
+++ lesson2/jwt_decode.py
@@ -5,8 +5,8 @@ def decode(token):
     payload += "=" * (-len(payload) % 4)
     return json.loads(base64.urlsafe_b64decode(payload.encode()))
 
-token = input("Enter JWT: ")
-data = decode(token)
-
-print("Username:", data.get("username"))
-print("Sub:", data.get("sub"))
+for name in ["TOKEN_B", "TOKEN_C"]:
+    data = decode(os.environ[name])
+    print("\n" + name)
+    print("username:", data.get("username"))
+    print("sub:", data.get("sub"))
